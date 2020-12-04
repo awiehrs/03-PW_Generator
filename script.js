42ec446f1f43003134e9b1153519c402b42c5b4f
Amanda <awiehrs.photo@gmail.com>
2020-12-03 21:34:17 -0500

    Added random generator functions

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+function generatePassword() {
+
+
+}
+
+function getRandomUpperCase(){
+  return String.fromCharCode(Math.floor(Math.random()*26)+65);
+ }
+ function getRandomLowerCase(){
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+function getRandomSymbol(){
+  var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
+  return symbol[Math.floor(Math.random()*symbol.length)];
+}
+
+// Write password to the #password input
+function writePassword() { 
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
+  return length = prompt("Choose a length for your password between 8-128 characters")
+  
 
 }
 
