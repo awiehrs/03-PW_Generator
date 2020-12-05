5038caa6478215b5f5e6e75af6b583790d2d7433
Amanda <awiehrs.photo@gmail.com>
2020-12-04 22:13:38 -0500

    Restructured password length selector, added criteria alert, restructured .js file to match linear progression of code

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-// Assignment Code
+// Button variable
 var generateBtn = document.querySelector("#generate");
 
+// Activates Generate Password button, runs the "writePassword" function
+generateBtn.addEventListener("click", writePassword);
 
-function generatePassword() {
+// Write password to the #password input
+function writePassword() { 
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
+  //passwordText.value = password;
+  var passwordLength = prompt("Choose a length for your password between 8-128 characters");
+    if (passwordLength < 8 ){
+      alert("Your password must be at least 8 characters long. Please try again.");
+      return;
+    }
+    else if (passwordLength > 128){
+      alert("Your password must be less than 128 characters. Please try again.");
+      return;
+    }
+   // Alert to give directions to user on how to select criteria
+  alert("The following prompts will ask for your password criteria. Please press 'OK' for 'Yes', and 'Cancel' for 'No'.");
 
 }
 
-function getRandomUpperCase(){
+
+
+
+/* function generatePassword() {
+
+}
+
+/*function getRandomUpperCase(){
   return String.fromCharCode(Math.floor(Math.random()*26)+65);
  }
  function getRandomLowerCase(){
@@ -19,18 +43,4 @@ function getRandomNumber(){
 function getRandomSymbol(){
   var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
   return symbol[Math.floor(Math.random()*symbol.length)];
-}
-
-// Write password to the #password input
-function writePassword() { 
-  //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
-
-  //passwordText.value = password;
-  return length = prompt("Choose a length for your password between 8-128 characters")
-  
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+} */
