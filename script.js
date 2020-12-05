9c9878b8f5882f22b4ff8665b9cd982a5fe83551
Amanda <awiehrs.photo@gmail.com>
2020-12-04 23:15:24 -0500

    updated the generatePassword function to combine with random generator functions inside of a while loop

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ generateBtn.addEventListener("click", writePassword);
 // Write password to the #password input
 function writePassword() { 
   //var password = generatePassword();
-  //var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
   //passwordText.value = password;
   var passwordLength = prompt("Choose a length for your password between 8-128 characters");
@@ -21,26 +21,43 @@ function writePassword() {
     }
    // Alert to give directions to user on how to select criteria
   alert("The following prompts will ask for your password criteria. Please press 'OK' for 'Yes', and 'Cancel' for 'No'.");
-
+  var upperCase = confirm("Do you want uppercase letters?");
+  var lowerCase = confirm("Do you want lowercase letters?");
+  var numbers = confirm("Do you want numbers?");
+  var specialChar = confirm("Do you want special characters? (Such as '$,%,!,^')");
+  //Alert for if user does not select any criteria, or to proceed with generating password
+  if (upperCase === false && lowerCase === false && numbers === false && specialChar === false)
+  { alert("Your password must have at least one criteria set. Please try again.")
+    passwordText.value="Please try again.";
+  }
+  else { 
+    passwordText.value ="";
+    passwordText.value = generatePassword(passwordLength,upperCase,lowerCase,specialChar,numbers);
+  }
 }
 
+//Function that generates random characters for password
+function generatePassword(passwordLength, upperCase, lowerCase, specialChar, numbers) {
+  var passwordText = "";
+  var userPasswordLength = 0;
 
-
-
-/* function generatePassword() {
-
+  while (userPasswordLength < passwordLength) {
+    if ((upperCase === true) && (userPasswordLength < passwordLength)) {
+      passwordText = passwordText + String.fromCharCode(Math.floor(Math.random()*26)+65);
+      userPasswordLength++;
+    }
+    if ((lowerCase === true) && (userPasswordLength < passwordLength)) {
+      passwordText = passwordText + String.fromCharCode(Math.floor(Math.random()*26)+97);
+      userPasswordLength++;
+    }
+    if ((numbers === true) && (userPasswordLength < passwordLength)) {
+      passwordText = passwordText + String.fromCharCode(Math.floor(Math.random()*10)+48);
+      userPasswordLength++;
+    }
+    if ((specialChar === true) && (userPasswordLength < passwordLength)) {
+      passwordText = passwordText + String.fromCharCode(Math.floor(Math.random()*15)+33);
+      userPasswordLength++;
+    }
+  }
+  return passwordText;
 }
-
-/*function getRandomUpperCase(){
-  return String.fromCharCode(Math.floor(Math.random()*26)+65);
- }
- function getRandomLowerCase(){
-  return String.fromCharCode(Math.floor(Math.random()*26)+97);
-}
-function getRandomNumber(){
-  return String.fromCharCode(Math.floor(Math.random()*10)+48);
-}
-function getRandomSymbol(){
-  var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
-  return symbol[Math.floor(Math.random()*symbol.length)];
-} */
